440f0105a7135c3405c14424b7c4ef69e609524e
shabaz0909 <shabazkhan@corewave.io>
2025-10-07 15:58:54 +0530

    Fix Issue

--- packages/Webkul/Shop/src/Resources/views/components/products/carousel.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/products/carousel.blade.php
@@ -5,116 +5,116 @@
 @pushOnce('scripts')
     <script type="text/x-template" id="v-products-carousel-template">
 
-                            <template v-if="isLoading">
-                                <x-shop::shimmer.products.scroll-product />
-                            </template>
+                                <template v-if="isLoading">
+                                    <x-shop::shimmer.products.scroll-product />
+                                </template>
 
-                            <template v-else>
-                                <section class="py-5 overflow-hidden">
-                                    <div class="container-fluid">
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="section-header d-flex flex-wrap justify-content-between my-5">
-                                                    <h2 class="section-title" v-html="title"></h2>
-                                                    <div class="d-flex align-items-center">
-                                                        <div class="swiper-buttons">
-                                                            <button class="swiper-prev products-carousel-prev btn btn-primary">❮</button> |
-                                                            <button class="swiper-next products-carousel-next btn btn-primary">❯</button>
+                                <template v-else>
+                                    <section class="py-5 overflow-hidden">
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="section-header d-flex flex-wrap justify-content-between my-5">
+                                                        <h2 class="section-title" v-html="title"></h2>
+                                                        <div class="d-flex align-items-center">
+                                                            <div class="swiper-buttons">
+                                                                <button class="swiper-prev products-carousel-prev btn btn-primary">❮</button> |
+                                                                <button class="swiper-next products-carousel-next btn btn-primary">❯</button>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
 
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="products-carousel swiper">
-                                                    <div class="swiper-wrapper">
-                                                        <div
-                                                            class="product-item swiper-slide"
-                                                            v-for="product in products"
-                                                            :key="product.index"
-                                                        >
-                                                            <span class="badge bg-primary rounded-pill position-absolute m-3" v-html="getPercentage(product?.prices?.regular?.price , product?.prices?.final?.price)"></span>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="products-carousel swiper">
+                                                        <div class="swiper-wrapper">
+                                                            <div
+                                                                class="product-item swiper-slide"
+                                                                v-for="product in products"
+                                                                :key="product.index"
+                                                            >
+                                                                <span class="badge bg-primary rounded-pill position-absolute m-3" v-html="getPercentage(product?.prices?.regular?.price , product?.prices?.final?.price)"></span>
 
-                                                            <span v-if="product.on_sale" class="badge bg-primary rounded-pill position-absolute m-3">Sale</span>
-
+                                                                <span v-if="product.on_sale" class="badge bg-primary rounded-pill position-absolute m-3">Sale</span>
 
 
-                                                            @if (core()->getConfigData('customer.settings.wishlist.wishlist_option'))
 
-                                                                <a  href="#"
-                                                                    class="btn-wishlist"
-                                                                    :style="product.is_wishlist ? 'background-color: white !important; color: #dc3545 !important;' : ''"
-                                                                    @click.prevent="toggleWishlist(product.id)" >
-                                                                    <i :class="product.is_wishlist ? 'fa-solid fa-heart' : 'fa-regular fa-heart'"></i>
+                                                                @if (core()->getConfigData('customer.settings.wishlist.wishlist_option'))
+
+                                                                    <a  href="#"
+                                                                        class="btn-wishlist"
+                                                                        :style="product.is_wishlist ? 'background-color: white !important; color: #dc3545 !important;' : ''"
+                                                                        @click.prevent="toggleWishlist(product.id)" >
+                                                                        <i :class="product.is_wishlist ? 'fa-solid fa-heart' : 'fa-regular fa-heart'"></i>
+                                                                    </a>
+
+                                                                @endif
+
+                                                             <figure class="product-image-wrapper">
+                                                                <a :href="'/' + product.url_key" :title="product.name">
+                                                                    <img
+                                                                        :src="product.base_image.original_image_url"
+                                                                        class="tab-image product-img"
+                                                                        :alt="product.name"
+                                                                    />
                                                                 </a>
-
-                                                            @endif
-
-                                                         <figure class="product-image-wrapper">
-                                                            <a :href="'/' + product.url_key" :title="product.name">
-                                                                <img
-                                                                    :src="product.base_image.original_image_url"
-                                                                    class="tab-image product-img"
-                                                                    :alt="product.name"
-                                                                />
-                                                            </a>
-                                                        </figure>
+                                                            </figure>
 
 
-                                                            <h6 v-text="product.name"></h6>
+                                                                <h6 v-text="product.name"></h6>
 
-                                                            <span class="rating">
-                                                                <svg width="24" height="24" class="text-primary">
-                                                                    <use xlink:href="#star-solid"></use>
-                                                                </svg>
-                                                                @{{ product.ratings.average || '0.0' }}
-                                                            </span>
-
-                                                            <div class="price d-flex">
-
-                                                                <span v-html="product.prices.final.formatted_price"></span>
-
-                                                                <small
-                                                                    v-if="product.prices.regular.price !== product.prices.final.price"
-                                                                    class="text-muted text-decoration-line-through small ms-2"
-                                                                >
-                                                                    @{{ product.prices.regular.formatted_price }}
-                                                                </small>
-                                                            </div>
-
-
-                                                            <div class="d-flex align-items-center justify-content-between mt-2">
-                                                                <button type="button" class="btn btn-success btn-sm add_to_cart explore-btn"
-                                                                :disabled="isStoring.addToCart"
-                                                                @click="addToCart(product)"
-                                                                >
-                                                                    Add to Cart
-                                                                    <svg width="16" height="16" viewBox="0 0 24 24">
-                                                                        <use xlink:href="#cart"></use>
+                                                                <span class="rating">
+                                                                    <svg width="24" height="24" class="text-primary">
+                                                                        <use xlink:href="#star-solid"></use>
                                                                     </svg>
-                                                                </button>
+                                                                    @{{ product.ratings.average || '0.0' }}
+                                                                </span>
 
-                                                                <button
-                                                                    type="button"
-                                                                    class="btn btn-success btn-sm buy_now explore-btn"
-                                                                    :disabled="isStoring.buyNow"
-                                                                    @click="is_buy_now=1;buyNow(product)"
-                                                                >
-                                                                    Buy Now
-                                                                </button>
+                                                                <div class="price d-flex">
+
+                                                                    <span v-html="product.prices.final.formatted_price"></span>
+
+                                                                    <small
+                                                                        v-if="product.prices.regular.price !== product.prices.final.price"
+                                                                        class="text-muted text-decoration-line-through small ms-2"
+                                                                    >
+                                                                        @{{ product.prices.regular.formatted_price }}
+                                                                    </small>
+                                                                </div>
+
+
+                                                                <div class="d-flex align-items-center justify-content-between mt-2">
+                                                                    <button type="button" class="btn btn-success btn-sm add_to_cart explore-btn"
+                                                                    :disabled="isStoring.addToCart"
+                                                                    @click="addToCart(product)"
+                                                                    >
+                                                                        Add to Cart
+                                                                        <svg width="16" height="16" viewBox="0 0 24 24">
+                                                                            <use xlink:href="#cart"></use>
+                                                                        </svg>
+                                                                    </button>
+
+                                                                    <button
+                                                                        type="button"
+                                                                        class="btn btn-success btn-sm buy_now explore-btn"
+                                                                        :disabled="isStoring.buyNow"
+                                                                        @click="is_buy_now=1;buyNow(product)"
+                                                                    >
+                                                                        Buy Now
+                                                                    </button>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </section>
-                            </template>
+                                    </section>
+                                </template>
 
-                        </script>
+                            </script>
 
     <script type="module">
         app.component('v-products-carousel', {
@@ -228,12 +228,13 @@
                                     message: response.data.message
                                 });
 
-                                showFlash("success", response.data.data.message);
+                                showFlash("success", response.data.message);
                                 this.$emitter.emit('add-flash', { type: 'success', message: response.data.message });
 
                                 if (response.data.redirect) {
                                     window.location.href = response.data.redirect;
                                 }
+                                window.location.reload();
                             } else {
                                 showFlash("warning", response.data.data.message);
                                 this.$emitter.emit('add-flash', { type: 'warning', message: response.data.data.message });
